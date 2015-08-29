1a1f2bbee1adedc0f80f2e9f684b7f862bb786e9
sapuit <sapnkp@gmail.com>
2015-08-29 16:44:14 +0700

    Volley

--- app/src/main/java/com/example/sapui/metarial/MainActivity.java
+++ app/src/main/java/com/example/sapui/metarial/MainActivity.java
@@ -21,6 +21,12 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.sapui.metarial.tabs.SlidingTabLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -153,6 +159,21 @@ public class MainActivity extends AppCompatActivity {
             if (bundle != null) {
                 textView.setText("This page select is " + bundle.getInt("position"));
             }
+
+            RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+            StringRequest request = new StringRequest(Request.Method.GET, "http://php.net/", new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Toast.makeText(getActivity(),"RESPONSE "+ response,Toast.LENGTH_SHORT).show();
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getActivity(),"ERROR "+ error,Toast.LENGTH_SHORT).show();
+                }
+            });
+            requestQueue.add(request);
+
             return layout;
         }
     }
